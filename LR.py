f2c11dafab31030a44aba5162a000727683b9b0b
fadwa <fadoamrii@gmail.com>
2022-09-21 10:18:04 -0600

    Check the relationship between two variables Hours and Scores

--- LR.py
+++ LR.py
@@ -16,3 +16,11 @@ from sklearn.metrics import mean_absolute_error, r2_score
 url="https://raw.githubusercontent.com/AdiPersonalWorks/Random/master/student_scores%20-%20student_scores.csv"
 df=pd.read_csv(url)
 print(df.head())
+
+#plot the Scatter plot to check relationship between Hours and Scores
+f,axs=plt.subplots(3,1,figsize=(10,10))
+f.text(0.5, 0.04, "Hours", ha='center')
+f.text(0.04, 0.5, "Scores", va='center', rotation='vertical')
+
+axs[0].scatter(df["Hours"],df["Scores"])
+print("Correlation =",df["Hours"].corr(df["Scores"]))
